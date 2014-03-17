e2a1ab0ee8846d8a23f7c78f048a4f0b64ed32f8
Murat Derya Ozen <muratdozen@gmail.com>
2014-03-17 21:58:09 +0200

    Initial commit

--- nodejs/routes/user.js
+++ nodejs/routes/user.js
@@ -1,3 +1,4 @@
+var userStore = require('./../data_store/user-store.js');
 /*
  * POST /follow
  */
@@ -5,6 +6,19 @@ exports.follow = function (req, res) {
     var followerId = req.body.from;
     var followeeId = req.body.to;
     console.log("user.follow started with fromUserId", followerId, "toUserId", followeeId);
+
+
+
+    var followeeUser = userStore.findById(followeeId) || userStore.newUserObject(followeeId);
+    var followers = followeeUser.followers;
+    if (followers.indexOf(followerId) == -1) {
+        followers.push(followerId);
+        followeeUser.followers = followers;
+    }
+    userStore.save(followeeUser);
+
+
+
 
     res.send(200);
 };
