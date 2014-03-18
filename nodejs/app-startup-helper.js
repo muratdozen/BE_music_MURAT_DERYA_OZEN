57e9b114252df873ecb49d36b71092a02c570bc2
Murat Derya Ozen <muratdozen@gmail.com>
2014-03-18 20:51:23 +0200

    Initial commit

---
+++ nodejs/app-startup-helper.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+var musicStore = require('./data_store/music-store.js');
+
+/**
+ * Load into musicStore the music objects from the json file located at the given path.
+ * @param filePath
+ */
+exports.loadMusicsJsonFile = function (filePath) {
+    console.log("Loading musics json file from '%s'", filePath);
+    fs.readFile(filePath, 'utf8', function (err, data) {
+        if (err) {
+            console.log('appStartupHelper.loadMusicsJsonFile failed to load musics json file:', err,
+                'Path was', filePath);
+            return;
+        }
+
+        data = JSON.parse(data);
+
+        for (var musicId in data) {
+            if (data.hasOwnProperty(musicId)) {
+                var list = data[musicId];
+                var musicObject = musicStore.newMusicObject(musicId, list);
+                musicStore.save(musicObject);
+            }
+        }
+    });
+}
+
