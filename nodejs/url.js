adcc55281619ceec06420b53c4b3d5491202a364
Murat Derya Ozen <muratdozen@gmail.com>
2014-03-29 21:52:56 +0200

    Issue #2 - Removed unused and unnecessary files

--- nodejs/url.js
+++ nodejs/url.js
@@ -1,4 +1,3 @@
-var routes = require('./routes');
 var user = require('./routes/user');
 
 /**
@@ -6,7 +5,6 @@ var user = require('./routes/user');
  * @param app
  */
 exports.configureRoutes = function (app) {
-    app.get('/', routes.index);
     app.post('/follow', user.follow);
     app.post('/listen', user.listen);
     app.get('/recommendations', user.recommendations);
